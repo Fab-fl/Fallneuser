61e672b2c0535ef7b023ede940f69fff2bdcd3b2
Luca1905 <71080491+Luca1905@users.noreply.github.com>
2022-09-09 17:13:04 +0200

    Update SlimyWand.java

--- src/main/java/com/fallenuser/falumod/item/SlimyWand.java
+++ src/main/java/com/fallenuser/falumod/item/SlimyWand.java
@@ -1,6 +1,5 @@
 package com.fallenuser.falumod.item;
 
-import net.minecraft.core.Position;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -8,12 +7,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.common.extensions.IForgeItemStack;
-import net.minecraftforge.registries.RegistryObject;
-
-import java.rmi.registry.Registry;
-
-import static com.fallenuser.falumod.item.ModItems.ITEMS;
 
 public class SlimyWand extends Item {
 
@@ -26,7 +19,7 @@ public class SlimyWand extends Item {
   public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
     if(!level.isClientSide()){
       player.sendSystemMessage(Component.literal("It worked!"));
-      float position = player.getPosition();
+      double x_cord = player.getX();
       player.setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(Items.STICK));
     }
     return super.use(level, player, hand);
